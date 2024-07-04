830bc053b8cb4ad5fb280c31370da5367611bde3
pandey-prakhar <prakharpandey1198@gmail.com>
2024-07-04 15:43:08 +0530

    this is version 2 to solve the issue using map and the problems that arises and remains

--- src/designpatterns/builder/App.java
+++ src/designpatterns/builder/App.java
@@ -1,9 +1,18 @@
 package designpatterns.builder;
 
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) {
-        Student st= new Student("prakhar",21,"M","engineering");
-        Student st2= new Student("prakhar","M");
+        //version 1
+//        Student st= new Student("prakhar",21,"M","engineering");
+//        Student st2= new Student("prakhar","M");
+        //version 2- map
+        HashMap<String,Object> map = new HashMap<>();//client creates this map and puts value inside as
+        map.put("name","prakhar");
+        map.put("age",23);
+        map.put("gender","male");
+        Student student = new Student(map);
 
 
     }
