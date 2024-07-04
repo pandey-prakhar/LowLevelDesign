b8617bab65ecea1fe496a77a0109a073af5b9452
pandey-prakhar <prakharpandey1198@gmail.com>
2024-07-04 23:25:42 +0530

    this is final version of builder and we can see how it helps creation of complex class so easy

--- src/designpatterns/builder/App.java
+++ src/designpatterns/builder/App.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 public class App {
     public static void main(String[] args) {
         //version 4
-        Builder builder = Student.getBuilder();
-        Student student = builder.build();
+//        Builder builder = Student.getBuilder();
+//        Student student = builder.build();
+        Student st2= Student.getBuilder().setGender("Male").build();
+
 
     }
 }
--- src/designpatterns/builder/Student.java
+++ src/designpatterns/builder/Student.java
@@ -2,19 +2,19 @@ package designpatterns.builder;
 
 public class Student {
     //lets assume there are multiple properties
-    int id;
-    String name;
-    int age;
-    String gender;
-    String major;
-    int score;
-    int psp;
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String major;
+    private int score;
+    private int psp;
 
     public static Builder getBuilder(){
         return new Builder();
     }
 
-    public Student(Builder builder){
+    private Student(Builder builder){
         //we can add any validations in this and in map as well.
         this.id= builder.getId();
         this.name= builder.getName();
@@ -24,8 +24,89 @@ public class Student {
         this.score= builder.getScore();
         this.psp= builder.getPsp();
     }
+
+     static class Builder {
+         private int id;
+         private String name;
+         private int age;
+         private String gender;
+         private String major;
+         private int score;
+         private int psp;
+
+         public Student build() {
+             // Inside build we can have all the validations
+             if (name == null || name.isEmpty()) {
+                 throw new IllegalArgumentException("Name cannot be null or empty");
+             }
+             if (age <= 5) {
+                 throw new IllegalArgumentException("Age must be greter than 5 to take admission");
+             }
+             return new Student(this);
+         }
+
+        public int getId() {
+            return id;
+        }
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public Builder setGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public String getMajor() {
+            return major;
+        }
+
+        public Builder setMajor(String major) {
+            this.major = major;
+            return this;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public Builder setScore(int score) {
+            this.score = score;
+            return this;
+        }
+
+        public int getPsp() {
+            return psp;
+        }
+
+        public void setPsp(int psp) {
+            this.psp = psp;
+        }
+    }
 }
-//this is first version of builder design pattern
-//IN this clients comes and then see he/she needs builder object then he will go inside builder then create
-//create builder object then pass
+
 
