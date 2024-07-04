83d8e64a3575a669ef79085e517388ae3b6a1916
pandey-prakhar <prakharpandey1198@gmail.com>
2024-07-04 16:10:38 +0530

    1st version of builder the most basic one

--- src/designpatterns/builder/App.java
+++ src/designpatterns/builder/App.java
@@ -4,16 +4,13 @@ import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) {
-        //version 1
-//        Student st= new Student("prakhar",21,"M","engineering");
-//        Student st2= new Student("prakhar","M");
-        //version 2- map
-        HashMap<String,Object> map = new HashMap<>();//client creates this map and puts value inside as
-        map.put("name","prakhar");
-        map.put("age",23);
-        map.put("gender","male");
-        Student student = new Student(map);
+        //version 3
+        Builder builder = new Builder();
+        builder.setAge(21);
+        builder.setGender("M");
+        builder.setName("Prakhar Pandey");
 
+        new Student(builder);
 
     }
 }
--- src/designpatterns/builder/Student.java
+++ src/designpatterns/builder/Student.java
@@ -1,7 +1,5 @@
 package designpatterns.builder;
 
-import java.util.Map;
-
 public class Student {
     //lets assume there are multiple properties
     int id;
@@ -12,52 +10,18 @@ public class Student {
     int score;
     int psp;
 
-/*version1 -normal condtructor drawbacks
- prone to errors
- we can have normal constrictor and it will create objects but what if we want object to be created
-with different set of parameters then the possible combinations are 2^noOfParams
- additionally client need to make sure the orderening of params in constructors.
-at some point constrictor overloading is not even possible.
-
- */
-//    public Student(String name, int age, String gender, String major) {
-//        this.name = name;
-//        this.age = age;
-//        this.gender = gender;
-//        this.major = major;
-//    }
-//
-//    public Student(String name, String gender) {
-//        this.name = name;
-//        this.gender = gender;
-//    }
-
-    /*
-    version 2- now we can rectify this multiple constructors by using a constructor taking map as param
-
-    problems with this code is this that it is error prone client map write as map.put("Name", "rakesh") or
-    map.put("age", "21")- typecast error
-     */
-    public Student( Map<String, Object> map){
-        /*
-        for easyness we are only checking for few properties
-         */
-        if(map.containsKey("name")){
-            this.name = (String) map.get("name");
-        }
-        if(map.containsKey("age")){
-            this.age = (int) map.get("age");
-        }
-        if(map.containsKey("gender")){
-            this.gender = (String) map.get("gender");
-        }
-        if(map.containsKey("major")){
-            this.major = (String) map.get("major");
-        }
-        //similarly for all others fields
-
-
+    public Student(Builder builder){
+        //we can add any validations in this and in map as well.
+        this.id= builder.getId();
+        this.name= builder.getName();
+        this.age= builder.getAge();
+        this.gender= builder.getGender();
+        this.major= builder.getMajor();
+        this.score= builder.getScore();
+        this.psp= builder.getPsp();
     }
-
 }
+//this is first version of builder design pattern
+//IN this clients comes and then see he/she needs builder object then he will go inside builder then create
+//create builder object then pass
 
